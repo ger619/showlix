5eb9cf754e0c78dc6f2676b7b4e4cfc15cc23878
@ger619 <abolger254@gmail.com>
2025-04-28 05:48:32 +0300

    update Devise configuration for User model and adjust Gemfile syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :trackable, :omniauthable,
+         :recoverable, :rememberable, :validatable, :confirmable, :lockable, omniauth_providers: %i[facebook],
+                                                                             timeoutable: false
 end

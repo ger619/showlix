f23ff91e2c3d2a197f9461c6990867b5364a0281
@ger619 <abolger254@gmail.com>
2025-04-29 12:26:06 +0300

    update Devise configuration for User model and adjust Gemfile syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :authenticate_user!, except: %i[new create]
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password, :profile_picture, :first_name, :last_name, :first_login)
+    end
+    devise_parameter_sanitizer.permit(:invite, keys: %i[email role])
+  end
+
 end
